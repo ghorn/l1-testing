188dc62053a9891e28df4f94af7f09f9de92f891
Greg Horn <greghorn@google.com>
2015-05-23 18:11:28 -0700

    l1 seems to run

--- src/L1/L1.hs
+++ src/L1/L1.hs
@@ -1,17 +1,41 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# Language TypeFamilies #-}
 {-# Language ScopedTypeVariables #-}
 {-# Language FlexibleContexts #-}
+{-# Language DeriveGeneric #-}
+{-# Language DeriveFunctor #-}
+{-# Language RecordWildCards #-}
 
-module L1 where
+module L1.L1
+       ( L1States(..)
+       , FullSystemState(..)
+       , L1Params(..)
+       , WQS(..)
+       , prepareL1
+       , integrate
+       , integrate'
+       ) where
+
+import GHC.Generics ( Generic, Generic1 )
+
+import qualified Numeric.GSL.ODE as ODE
+import qualified Numeric.LinearAlgebra.Data as D
+import qualified Data.Vector as V
+import qualified Data.Vector.Storable as SV
 
 import Casadi.CMatrix ( CMatrix )
+import Casadi.SX ( SX )
+import Casadi.DMatrix ( DMatrix )
+import Casadi.MX ( MX )
 import Casadi.Overloading ( SymOrd(..) )
 
 import Dyno.Vectorize
-import Dyno.View.M
+import Dyno.View.M ( M, ms, mm, trans, uncol, col, hsplitTup )
 import Dyno.View.Viewable
 import Dyno.View.JV
-import Dyno.View.View hiding ( J )
+import Dyno.View.Fun
+import Dyno.View.FunJac
+import Dyno.View.View
 
 dot :: (View f, CMatrix a) => M f (JV Id) a -> M f (JV Id) a -> M (JV Id) (JV Id) a
 dot x y = trans x `mm` y
@@ -69,8 +93,9 @@ gradfproj etheta thetamax theta =
 gt :: (Num a, SymOrd a) => a -> a -> a
 gt x y = 1 - (x `leq` y)
 
-lt :: (Num a, SymOrd a) => a -> a -> a
-lt x y = 1 - (x `geq` y)
+-- todo(move this and its friend to SymOrd (casadi bindings))
+--lt :: (Num a, SymOrd a) => a -> a -> a
+--lt x y = 1 - (x `geq` y)
 
 proj :: forall x a
         . (View x, Viewable a, CMatrix a, SymOrd (M x (JV Id) a))
@@ -109,55 +134,172 @@ scale s m = m `ms` (uncol s)
 Discrete L1 controller step
 
 -}
+
+
 data L1States x a =
   L1States
-  { l1sXhat :: M x (JV Id) a
-  , l1sU :: S a
-  , l1sOmega :: S a -- M v (JV Id) a
-  , l1sTheta :: M x (JV Id) a
-  , l1sSigma :: S a -- M s (JV Id) a
-  }
+  { l1sXhat :: x a
+  , l1sU :: a
+  , l1sWqsHat :: WQS x a
+  } deriving (Functor, Generic, Generic1, Show)
+instance Vectorize x => Vectorize (L1States x)
+
+data WQS x a =
+  WQS
+  { wqsOmega :: a
+  , wqsTheta :: x a
+  , wqsSigma :: a
+  } deriving (Functor, Generic, Generic1, Show)
+instance Vectorize x => Vectorize (WQS x)
+
+data FullSystemState x a =
+  FullSystemState
+  { ffsX :: x a
+  , ffsWQS :: WQS x a
+  } deriving (Functor, Generic, Generic1)
+instance Vectorize x => Vectorize (FullSystemState x)
+
+data FullL1State x a =
+  FullL1State
+  { controllerState :: J (JV (L1States x)) a
+  , systemState :: J (JV (FullSystemState x)) a
+  } deriving (Generic, Generic1)
+instance Vectorize x => View (FullL1State x)
+
+data L1Params x a =
+  L1Params
+  { l1pETheta0 :: S a
+  , l1pOmegaMax :: S a
+  , l1pSigmaMax :: S a
+  , l1pThetaMax :: S a
+  , l1pGamma :: S a
+  , l1pKg :: S a
+  , l1pP :: M x x a
+  , l1pW :: S a
+  } deriving Functor
+
 
 type S a = M (JV Id) (JV Id) a
 
-l1step ::
+prepareL1 ::
+  forall x u
+  . (Vectorize x, u ~ Id)
+  => (FullSystemState x (J (JV Id) SX) -> J (JV Id) SX -> x (J (JV Id) SX))
+  -> L1Params (JV x) MX -- todo(symbolic leak)
+  -> IO (FullSystemState x Double -> L1States x Double -> Double -> IO (L1States x Double, x Double))
+prepareL1 userOde l1params = do
+  let f :: JacIn (JTuple (JV x) (JV u)) (J (JV (WQS x))) SX -> JacOut (JV x) (J JNone) SX
+      f (JacIn xu wqs) = JacOut (catJV' xdot) (cat JNone)
+        where
+          xdot :: x (J (JV Id) SX)
+          xdot = userOde ffs u
+
+          ffs :: FullSystemState x (J (JV Id) SX)
+          ffs =
+            FullSystemState
+            { ffsX = splitJV' x
+            , ffsWQS = splitJV' wqs
+            }
+
+          JTuple x u = split xu
+
+  sxF <- toSXFun "user ode" f
+  userJac <- toFunJac sxF
+
+  let fullOde :: JTuple (FullL1State x) (JV Id) MX -> JTuple (JV (L1States x)) (JV x) MX
+      fullOde (JTuple fullL1States r) = JTuple l1dot x'
+        where
+          l1dot = ddtL1States l1params dx'dx dx'du (col r) x (catJV' controllerState')
+
+          FullL1State{..} = split fullL1States
+          controllerState'@(L1States{..}) = splitJV' controllerState
+
+          --jacIn :: JacIn (JTuple f0 (JV Id)) (WQS x) (J (JV Id) MX)
+          jacIn = JacIn (cat (JTuple (catJV' l1sXhat) (catJV' (Id l1sU)))) (catJV' l1sWqsHat)
+          dx'dxu :: M (JV x) (JTuple (JV x) (JV u)) MX
+          x' :: J (JV x) MX
+          Jac dx'dxu x' _ = call userJac jacIn
+
+          dx'dx :: M (JV x) (JV x) MX
+          dx'du :: M (JV x) (JV u) MX
+          (dx'dx, dx'du) = hsplitTup dx'dxu
+
+          x :: M (JV x) (JV Id) MX
+          x = col $ catJV' (ffsX (splitJV' systemState))
+
+  fullOdeMX <- toMXFun "full ode with l1" fullOde
+
+  let retFun :: FullSystemState x Double -> L1States x Double -> Double
+                -> IO (L1States x Double, x Double)
+      retFun ffs l1States r = do
+        let fullL1States :: FullL1State x DMatrix
+            fullL1States =
+              FullL1State
+              { controllerState = v2d $ catJV l1States
+              , systemState = v2d $ catJV ffs
+              }
+            input = JTuple (cat fullL1States) (v2d (catJV (Id r)))
+        JTuple ret x' <- eval fullOdeMX input
+        return (splitJV (d2v ret), splitJV (d2v x'))
+
+  return retFun
+
+
+ddtL1States ::
   forall a x
-  . (View x, Viewable a, CMatrix a)
-  => S a
+  . (Vectorize x, Viewable a, CMatrix a)
+  => L1Params (JV x) a
+  -> M (JV x) (JV x) a -- am
+  -> M (JV x) (JV Id) a -- b
   -> S a
-  -> S a
-  -> S a
-  -> S a
-  -> S a
-  -> M x x a -- p
-  -> S a
-  -> M x x a -- am
-  -> M x (JV Id) a -- b
-  -> S a
-  -> M x (JV Id) a
-  -> L1States x a -> L1States x a
-l1step etheta0 omegamax sigmamax thetamax gamma kg p w am b r x
-  (L1States xhat u omegahat thetahat sigmahat) =
-    L1States xhatdot udot omegahatdot thetahatdot sigmahatdot
+  -> M (JV x) (JV Id) a
+  -> J (JV (L1States x)) a -> J (JV (L1States x)) a
+ddtL1States L1Params{..} am b r x l1states =
+  catJV' $ L1States (splitJV' (uncol xhatdot)) (unId (splitJV' (uncol udot))) wqsDot
   where
+    L1States xhat0 u0 wqsHat' = splitJV' l1states
+
+    xhat :: M (JV x) (JV Id) a
+    xhat = col (catJV' xhat0)
+
+    u :: M (JV Id) (JV Id) a
+    u = col u0
+
+    wqsDot :: WQS x (J (JV Id) a)
+    wqsDot =
+      WQS
+      { wqsOmega = unId $ splitJV' (uncol omegahatdot)
+      , wqsTheta =        splitJV' (uncol thetahatdot)
+      , wqsSigma = unId $ splitJV' (uncol sigmahatdot)
+      }
+
+    wqsHat :: WQS x (J (JV Id) a)
+    wqsHat = wqsHat'
+    omegahat :: M (JV Id) (JV Id) a
+    omegahat = col (catJV' (Id (wqsOmega wqsHat)))
+    thetahat :: M (JV x) (JV Id) a
+    thetahat = col (catJV' (wqsTheta wqsHat))
+    sigmahat :: M (JV Id) (JV Id) a
+    sigmahat = col (catJV' (Id (wqsSigma wqsHat)))
+
     -- Compute error between reference model and true state
-    xtilde :: M x (JV Id) a
+    xtilde :: M (JV x) (JV Id) a
     xtilde = xhat - x
     -- Update parameter estimates.  The estimate derivatives we
     -- compute here will be used to form the next step's estimates; we
     -- use the values we receive as arguments for everything at this
     -- step.
     xtpb :: S a
-    xtpb = (-(trans xtilde)) `mm` p `mm` b
+    xtpb = (-(trans xtilde)) `mm` l1pP `mm` b
 
     gp :: View f => S a -> M f (JV Id) a -> M f (JV Id) a -> M f (JV Id) a
-    gp somethingMax th sig = gamma `scale` proj etheta0 somethingMax th sig
+    gp somethingMax th sig = l1pGamma `scale` proj l1pETheta0 somethingMax th sig
 
     omegahatdot,sigmahatdot :: S a
-    omegahatdot = gp omegamax omegahat (xtpb `scale` u)
-    sigmahatdot = gp sigmamax sigmahat xtpb
-    thetahatdot :: M x (JV Id) a
-    thetahatdot = gp thetamax thetahat (xtpb `scale` x)
+    omegahatdot = gp l1pOmegaMax omegahat (xtpb `scale` u)
+    sigmahatdot = gp l1pSigmaMax sigmahat xtpb
+    thetahatdot :: M (JV x) (JV Id) a
+    thetahatdot = gp l1pThetaMax thetahat (xtpb `scale` x)
     -- Update reference model state using the previous values.  The
     -- 'xhat' value we receive should be the model's prediction (using
     -- the previous xhat and xhatdot) for the true state 'x' at this
@@ -166,11 +308,46 @@ l1step etheta0 omegamax sigmamax thetamax gamma kg p w am b r x
     eta :: S a
     eta = omegahat * u + thetahat `dot` x + sigmahat
 
-    xhatdot :: M x (JV Id) a
+    xhatdot :: M (JV x) (JV Id) a
     xhatdot = am `mm` xhat + eta `scale` b
     -- Update the reference LPF state
     e :: S a
-    e = kg * r - eta
+    e = l1pKg * r - eta
 
     udot :: S a
-    udot = dstep w e u
+    udot = dstep l1pW e u
+
+integrate :: Vectorize x => (x Double -> x Double) -> Double -> x Double -> x Double
+integrate f h x0 = devectorize $ sv $ last sol
+  where
+    vs :: V.Vector Double -> SV.Vector Double
+    vs = SV.fromList .  V.toList
+    sv :: SV.Vector Double -> V.Vector Double
+    sv =  V.fromList . SV.toList
+
+    sol = D.toRows $
+          ODE.odeSolveV
+          ODE.MSAdams
+          h 1e-7 1e-5 f'
+          (vs (vectorize x0))
+          (SV.fromList [0, h])
+    f' :: Double -> SV.Vector Double -> SV.Vector Double
+    f' _ x = vs $ vectorize $ f (devectorize (sv x))
+
+
+integrate' :: Vectorize x => (x Double -> x Double) -> Double -> [Double] -> x Double -> [x Double]
+integrate' f h times x0 = map (devectorize . sv) sol
+  where
+    vs :: V.Vector Double -> SV.Vector Double
+    vs = SV.fromList .  V.toList
+    sv :: SV.Vector Double -> V.Vector Double
+    sv =  V.fromList . SV.toList
+
+    sol = D.toRows $
+          ODE.odeSolveV
+          ODE.MSAdams
+          h 1e-7 1e-5 f'
+          (vs (vectorize x0))
+          (SV.fromList times)
+    f' :: Double -> SV.Vector Double -> SV.Vector Double
+    f' _ x = vs $ vectorize $ f (devectorize (sv x))
--- src/L1/L1Test.hs
+++ src/L1/L1Test.hs
@@ -4,29 +4,39 @@
 {-# Language DeriveFunctor #-}
 
 module L1.L1Test
-       ( RoboX(..)
-       , RoboU(..)
+       ( main
+       , RoboX(..)
+       , SimStates(..)
        , ddtRoboX
        ) where
 
 import GHC.Generics ( Generic, Generic1 )
 
+import System.IO.Unsafe ( unsafePerformIO )
+
+import qualified Numeric.LinearAlgebra.HMatrix as HMat
 import Linear
 
+--import Casadi.DMatrix ( DMatrix )
+import Casadi.MX ( MX )
 import Dyno.Vectorize
+import Dyno.View.JV
+import Dyno.View.M ( fromHMat )
+
+import L1.L1
 
 data RoboX a =
   RoboX
   { xPos :: a
   , xVel :: a
-  } deriving (Functor, Generic, Generic1)
+  } deriving (Functor, Generic, Generic1, Show)
 instance Vectorize RoboX
 
-data RoboU a =
-  RoboU
-  { uTorque :: a
-  } deriving (Functor, Generic, Generic1)
-instance Vectorize RoboU
+--data RoboU a =
+--  RoboU
+--  { uTorque :: a
+--  } deriving (Functor, Generic, Generic1)
+--instance Vectorize RoboU
 
 --data RoboTheta a =
 --  RoboTheta
@@ -35,8 +45,9 @@ instance Vectorize RoboU
 --  } deriving (Functor, Generic, Generic1)
 --instance Vectorize RoboTheta
 
-ddtRoboX :: Floating a => RoboX a -> RoboU a -> RoboX a -> a -> a -> RoboX a
-ddtRoboX x@(RoboX p v) (RoboU u) theta sigmaBar omegaBar =
+ddtRoboX :: Floating a => FullSystemState RoboX a -> a -> RoboX a
+ddtRoboX (FullSystemState x@(RoboX p v) (WQS omegaBar theta sigmaBar)) u  =
+--ddtRoboX x@(RoboX p v) u theta sigmaBar omegaBar =
   RoboX
   { xPos = v
   , xVel = (u + m*g*r*(cos p)/2 + sigmaBar + x `dot` theta) * omegaBar
@@ -45,3 +56,59 @@ ddtRoboX x@(RoboX p v) (RoboU u) theta sigmaBar omegaBar =
     m = 1
     g = 9.8
     r = 0.5
+
+l1params :: L1Params (JV RoboX) MX
+l1params =
+  L1Params
+  { l1pETheta0 = 0.1
+  , l1pOmegaMax = 10
+  , l1pSigmaMax = 10
+  , l1pThetaMax = 5
+  , l1pGamma = 100e3
+  , l1pKg = 1
+  , l1pP = fromHMat $
+           HMat.fromLists
+           [ [0.812564928052004, -0.359997120023040]
+           , [-0.359997120023040, 0.308568960019748]
+           ]
+  , l1pW = 1
+  }
+
+data SimStates x a =
+  SimStates
+  { ssX :: x a
+  , ssL1 :: L1States x a
+  } deriving (Functor, Generic, Generic1, Show)
+instance Vectorize x => Vectorize (SimStates x)
+
+main :: IO ()
+main = do
+  lol <- prepareL1 ddtRoboX l1params
+  let x0 :: RoboX Double
+      x0 = RoboX 1 2
+
+      l0 :: L1States RoboX Double
+      l0 =
+        L1States
+        { l1sXhat = fill 1
+        , l1sU = 0
+        , l1sWqsHat = fill 0.1
+        }
+      reference = 0
+
+      wqs0 :: WQS RoboX Double
+      wqs0 = fill 0.1
+
+  let dfdt :: WQS RoboX Double -> Double -> SimStates RoboX Double -> SimStates RoboX Double
+      dfdt wqs r (SimStates x l1) = unsafePerformIO $ do
+        let fss =
+              FullSystemState
+              { ffsX = x
+              , ffsWQS = wqs
+              }
+        (l1', x') <- lol fss l1 r
+        return (SimStates x' l1')
+
+      sols = integrate' (dfdt wqs0 reference) 0.01 [0,0.01..0.1] (SimStates x0 l0)
+  mapM_ print sols
+  return ()
