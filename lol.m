597a0e44642f3b0230df763182fc18b07b1e049f
Matthew Peddie <mpeddie@gmail.com>
2015-06-01 15:19:43 +1000

    Clean up plotting a bit; plot error against actual reference input

--- lol.m
+++ lol.m
@@ -2,22 +2,47 @@ clear all
 clc; clf;
 fuckme
 
-subplot(411)
-plot(time, [ret.ssX.xPos; ret.ssX.xVel]','.-')
-legend('pos','vel')
+subplot(411);
+hold on;
+grid on;
+title('System states');
+xlabel('Time (s)');
+ylabel('State (rad, ^r^a^d/_s)');
+xlim([0 time(end)]);
+plot(time, [ret.ssX.xPos; ret.ssX.xVel; r]');
+legend('pos','vel', 'position reference');
 
-subplot(412)
-plot(time, [ret.ssX.xPos - ret.ssL1.l1sXhat.xPos; ret.ssX.xVel - ret.ssL1.l1sXhat.xVel]','.-')
-legend('pos err','vel err')
+subplot(412);
+hold on;
+grid on;
+title('Tracking errors');
+xlabel('Time (s)');
+ylabel('Error (rad)');
+xlim([0 time(end)]);
+plot(time, [abs(ret.ssX.xPos - r); abs(ret.ssX.xPos - ret.ssL1.l1sXhat.xPos)]');
+legend('position err from reference input', ...
+       'position err from desired system response');
 
-subplot(413)
-plot(time, [ret.ssL1.l1sU]','.-')
-legend('u')
+subplot(413);
+hold on;
+grid on;
+title('Control input');
+xlabel('Time (s)');
+ylabel('Input torque (N-m)');
+xlim([0 time(end)]);
+plot(time, [ret.ssL1.l1sU]');
+legend('u');
 
-subplot(414)
-plot(time, [ret.ssL1.l1sWqsHat.wqsOmega]','.-')
-hold on
-plot(time, [ret.ssL1.l1sWqsHat.wqsSigma]','.-')
-plot(time, [ret.ssL1.l1sWqsHat.wqsTheta.xPos]','.-')
-plot(time, [ret.ssL1.l1sWqsHat.wqsTheta.xVel]','.-')
-legend('omega','sigma','thetaPos','thetaVel')
+subplot(414);
+hold on;
+grid on;
+title('Parameter estimates');
+xlabel('Time (s)');
+ylabel('Coefficient value');
+xlim([0 time(end)]);
+plot(time, [ret.ssL1.l1sWqsHat.wqsOmega; ...
+            ret.ssL1.l1sWqsHat.wqsSigma; ...
+            ret.ssL1.l1sWqsHat.wqsTheta.xPos; ...
+            ret.ssL1.l1sWqsHat.wqsTheta.xVel; ...
+           ]');
+legend('omega','sigma','thetaPos','thetaVel');
